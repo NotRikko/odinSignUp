c4edcea6ab9538202c9be282bf33b4918a31de54
Rikko <bulletbill51@yahoo.com>
2023-10-23 15:35:31 -0700

    Added password confimration Added javascript event listeners to confirm passwords are equal Added pattern regex to require more secure passwords Added some styling changes for aesthetic purposes

---
+++ java.js
@@ -0,0 +1,21 @@
+let confirmPassword = document.querySelector("#confirmPassword");
+let password = document.querySelector("#password");
+
+confirmPassword.addEventListener("input",() => {
+    if (password.value === confirmPassword.value) {
+        confirmPassword.style.border ="1px solid green";
+    }
+    else confirmPassword.style.border ="1px solid red";
+}) 
+
+confirmPassword.addEventListener("focus",() => {
+    if (password.value === confirmPassword.value) {
+        confirmPassword.style.border ="1px solid green";
+    }
+    else confirmPassword.style.border ="1px solid red";
+}) 
+
+confirmPassword.addEventListener("blur", () => {
+    confirmPassword.style.border = "none";
+    confirmPassword.style.borderBottom = "1px solid white";
+})
